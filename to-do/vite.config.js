e6eec445c0b1ea4b25290c42b36e9779dba48194
TheMateeus <105858907+TheMateeus@users.noreply.github.com>
2023-08-04 20:40:30 +0000

    Configuracoeas de deploy

--- to-do/vite.config.js
+++ to-do/vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+// https://viteclearjs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/todo-react-vite-deploy"
 })
